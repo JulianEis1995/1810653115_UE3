a97f436b3a3cee9327a263fb2dd193e088c08b8b
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-29 11:06:23 +0100

    Aufgabe 3

--- src/Aufgabe2_1810653115.java
+++ src/Aufgabe2_1810653115.java
@@ -1,11 +1,13 @@
-public class Aufgabe_1810653115
+public class Aufgabe2_1810653115
 {
     private String a;
 
     public static void main(String[] args)
     {
-        partialString("hans");
-        partialString("j");
+        //Ausgabe von 3 Buchstaben
+        partialString("alfre");
+        //Ausgabe eines Fehlers
+        partialString("ja");
 
     }
 
@@ -19,6 +21,11 @@ public class Aufgabe_1810653115
         {
             //Länger herausfinden bspw. hans = 4 Zeichen
             int l = a.length();
+
+            //Nebenrechnung -> Ergebnis 2 -> fängt bei Char 2 an
+            int ausgabe = 5/2;
+            System.out.println(ausgabe);
+
             //Substring = starten ab best. Position
             // hier hans = 4 Zeichen / 2 = 2 Zeichen -> Start 2 Zeichen
             // zweite Hälfte vom String
---
+++ src/Taschenrechner_Aufgabe3_1810653115.java
@@ -0,0 +1,65 @@
+public class Taschenrechner_Aufgabe3_1810653115
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Addition");
+        Addition(1, 2 );
+        Addition(2.5,3.5);
+
+        System.out.println("Subtraktion");
+        Subtraktion(2, 1);
+        Subtraktion(2.5, 1.5);
+
+        System.out.println("Multiplikation");
+        Multiplikation(1 , 2);
+        Multiplikation(2.5, 3.5);
+
+        System.out.println("Division");
+        Division(4, 2);
+        Division(4.5, 3.5);
+    }
+    //Addition
+    public static void Addition(int a, int b)
+    {
+        int ergebnis = a + b;
+        System.out.println(ergebnis);
+    }
+    public static void Addition (double a, double b)
+    {
+        double ergebnis = a + b;
+        System.out.println(ergebnis);
+    }
+    //Subtraktion
+    public static void Subtraktion(int a, int b)
+    {
+        int ergebnis = a - b;
+        System.out.println(ergebnis);
+    }
+    public static void Subtraktion (double a, double b)
+    {
+        double ergebnis = a - b;
+        System.out.println(ergebnis);
+    }
+    //Multiplikation
+    public static void Multiplikation(int a, int b)
+    {
+        int ergebnis = a * b;
+        System.out.println(ergebnis);
+    }
+    public static void Multiplikation(double a, double b)
+    {
+        double ergebnis = a * b;
+        System.out.println(ergebnis);
+    }
+    //Division
+    public static void Division(int a, int b)
+    {
+        int ergebnis = a / b;
+        System.out.println(ergebnis);
+    }
+    public static void Division(double a, double b)
+    {
+        double ergebnis = a / b;
+        System.out.println(ergebnis);
+    }
+}
